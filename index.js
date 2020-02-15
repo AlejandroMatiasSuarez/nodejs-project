feaca7ddb740b80bd34cdd1710b621e76f0066ca
alexlecco <ale_10_88@hotmail.com>
2020-02-15 19:47:31 -0300

    Resolved conflic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -15,3 +16,38 @@ myApp.get('/test', (request, response) => {
 });
 
 console.log('Hello World'); 
+=======
+const mongoose = require('mongoose');
+const myApp = require('./myApp');
+const port = 3001;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/test_com_8_3')
+    .then(() => {
+        console.log('Connected successfully ...');
+        // Define Schema
+        const userScheme = mongoose.Schema({
+            name: String,
+            lastname: String,
+            email: String
+        });
+        // Pasa Schema a model
+        const User = mongoose.model('User', userScheme);
+        // Nueva instancia del documento
+        const matiasUser = new User({
+            _id: mongoose.Types.ObjectId(),
+            name: 'Matías',
+            lastname: 'Aybar',
+            email: 'matiasaybar89@gmail.com',
+        });
+        // Guardo en la base de datos
+        matiasUser.save(() => {
+            console.log('User was added');
+        });
+        myApp.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}...`);
+        });
+    })
+    .catch(error => console.log(error));
+>>>>>>> 39d6479a1e32310a8d3865618f3a72a98f525a19
